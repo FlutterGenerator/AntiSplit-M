43dac40a70ee3790fa02c59ddf89fe1e58b9ad5a
tojik_proof_93 <elementman50@yandex.com>
2026-01-14 20:33:56 +0500

    2.2.7

--- app/src/main/java/com/abdurazaaqmohammed/AntiSplit/main/MainActivity.java
+++ app/src/main/java/com/abdurazaaqmohammed/AntiSplit/main/MainActivity.java
@@ -551,7 +551,7 @@ public class MainActivity extends AppCompatActivity {
                         currentVer = null;
                     }
                     boolean newVer = false;
-                    char[] curr = TextUtils.isEmpty(currentVer) ? new char[] {'2', '2', '6'} : currentVer.replace(".", "").toCharArray();
+                    char[] curr = TextUtils.isEmpty(currentVer) ? new char[] {'2', '2', '7'} : currentVer.replace(".", "").toCharArray();
                     char[] latest = latestVersion.replace(".", "").toCharArray();
 
                     int maxLength = Math.max(curr.length, latest.length);
@@ -774,7 +774,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 currentVer = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
             } catch (Exception ex) {
-                currentVer = "2.2.6";
+                currentVer = "2.2.7";
             }
             fullLog.append(currentVer).append('\n')
                     .append("Storage permission granted: ")
